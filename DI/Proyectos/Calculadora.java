8ceb48b90e9466bbc66962ccb586fc2112faf854
John.V <lhonvera2000@gmail.com>
2026-09-20 14:29:43 +0200

    Inicializo el escaner y creo metodos repetivos que usare en el main

--- DI/Proyectos/Calculadora.java
+++ DI/Proyectos/Calculadora.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Calculadora {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static int leerInt(String mensaje){
+        System.out.print(mensaje);
+        return scanner.nextInt();
+    }
+    public static int opcion(){
+        return leerInt("OPCIÓN: ");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Crear clase inicial");
+
     }
 }
